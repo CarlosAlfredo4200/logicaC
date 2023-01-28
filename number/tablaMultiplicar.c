436a8de0bc23fa26308d72c989046eef56d32ede
Carlos <alfredomontoyagit@gmail.com>
2023-01-28 14:15:43 -0500

    Inicio

---
+++ number/tablaMultiplicar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    int res;
+    printf("Ingrese el número de la tabla : ");
+    scanf("%d",&num);
+
+    for (int i = 0; i <= 10; i++)
+    {
+        res = num * i;
+        printf("\n%d * %d es = %d", num, i, res);
+    }
+    
+
+    return 0;
+}
