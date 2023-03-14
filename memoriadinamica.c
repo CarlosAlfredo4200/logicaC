3c68d156e762dd0a8b8fe413ed6cacfe12344717
Carlos <alfredomontoyagit@gmail.com>
2023-03-13 20:19:47 -0500

    malloc

--- memoriadinamica.c
+++ memoriadinamica.c
@@ -4,6 +4,11 @@
 int main()
 {
     
-    float* = valores = (float*)  malloc(4000 * sizeof(float));
+    // float* = valores = (float*)  malloc(4000 * sizeof(float));
+
+    int n = 98;
+    int *p = &n;
+
+    printf("%d", n);
     return 0;
 }
